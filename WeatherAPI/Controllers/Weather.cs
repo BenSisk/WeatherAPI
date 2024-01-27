95d5c393aaea548029e8138c8bef2680ff6f7604
BenSisk <43730029+BenSisk@users.noreply.github.com>
2024-01-27 18:20:06 +0000

    Try Catch Error Handling, just in case

--- WeatherAPI/Controllers/Weather.cs
+++ WeatherAPI/Controllers/Weather.cs
@@ -15,26 +15,34 @@ namespace WeatherAPI.Controllers
         [Route("[controller]")]
         public async Task<IActionResult> Get(double? Long, double? Lat, string? TempUnit, string? API)
         {
-            // if null, default to london
-            double Longitude = Long is not null ? (double)Long : -0.11489;
-            double Latitude = Lat is not null ? (double)Lat : 51.51418;
+            try
+            {
+                // if null, default to london
+                double Longitude = Long is not null ? (double)Long : -0.11489;
+                double Latitude = Lat is not null ? (double)Lat : 51.51418;
 
-            TempUnit = TempUnit is not null ? TempUnit.ToLower() : "c";
+                TempUnit = TempUnit is not null ? TempUnit.ToLower() : "c";
 
-            if (Lat < -90 || Lat > 90 || Long < -180 || Long > 180) return BadRequest("Invalid latitude or longitude");
-            if (TempUnit != "c" && TempUnit != "f" && TempUnit != "k") return BadRequest("Invalid temperature unit. Please use 'C', 'K' or 'F'");
-            TempUnit = TempUnit == "c" || TempUnit == "f"  || TempUnit == "k" ? TempUnit : "c";
+                if (Lat < -90 || Lat > 90 || Long < -180 || Long > 180) return BadRequest("Invalid latitude or longitude");
+                if (TempUnit != "c" && TempUnit != "f" && TempUnit != "k") return BadRequest("Invalid temperature unit. Please use 'C', 'K' or 'F'");
+                TempUnit = TempUnit == "c" || TempUnit == "f"  || TempUnit == "k" ? TempUnit : "c";
 
 
-            // default to OpenWeatherMap, weatherbit is an optional variable
-            IExternalWeatherAPI ChosenAPI;
-            if (API is not null && API.ToLower() == "weatherbit") ChosenAPI = WeatherBit.GetInstance();
-            else ChosenAPI = OpenWeatherMap.GetInstance();
+                // default to OpenWeatherMap, weatherbit is an optional variable
+                IExternalWeatherAPI ChosenAPI;
+                if (API is not null && API.ToLower() == "weatherbit") ChosenAPI = WeatherBit.GetInstance();
+                else ChosenAPI = OpenWeatherMap.GetInstance();
 
-            WeatherAPIData data = await WeatherAPIParent.Query(ChosenAPI, Longitude, Latitude, TempUnit);
+                WeatherAPIData data = await WeatherAPIParent.Query(ChosenAPI, Longitude, Latitude, TempUnit);
 
-            if (data is not null) return Ok(data);
-            else return NotFound("Error 404: The selected API may be temporarily unavailable");
+                if (data is not null) return Ok(data);
+                else return NotFound("Error 404: The selected API may be temporarily unavailable");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest(e.Message);
+            }
         }
 
 
@@ -43,22 +51,32 @@ namespace WeatherAPI.Controllers
         [Route("weather/date-range&startdate={StartDate}&enddate={EndDate}")]
         public async Task<IActionResult> GetDateRange(double? Long, double? Lat, string? TempUnit, DateTime StartDate, DateTime EndDate)
         {
-            // if null, default to london
-            double Longitude = Long is not null ? (double)Long : -0.11489;
-            double Latitude = Lat is not null ? (double)Lat : 51.51418;
+            try
+            {
+                // if null, default to london
+                double Longitude = Long is not null ? (double)Long : -0.11489;
+                double Latitude = Lat is not null ? (double)Lat : 51.51418;
 
-            TempUnit = TempUnit is not null ? TempUnit.ToLower() : "c";
+                TempUnit = TempUnit is not null ? TempUnit.ToLower() : "c";
 
-            if (Lat < -90 || Lat > 90 || Long < -180 || Long > 180) return BadRequest("Invalid latitude or longitude");
-            if (TempUnit != "c" && TempUnit != "f" && TempUnit != "k") return BadRequest("Invalid temperature unit. Please use 'C', 'K' or 'F'");
+                if (Lat < -90 || Lat > 90 || Long < -180 || Long > 180) return BadRequest("Invalid latitude or longitude");
+                if (TempUnit != "c" && TempUnit != "f" && TempUnit != "k") return BadRequest("Invalid temperature unit. Please use 'C', 'K' or 'F'");
 
+                TempUnit = TempUnit == "c" || TempUnit == "f" || TempUnit == "k" ? TempUnit : "c";
 
-            TempUnit = TempUnit == "c" || TempUnit == "f" || TempUnit == "k" ? TempUnit : "c";
+                if (StartDate > DateTime.Now) return BadRequest("Start date cannot be in the future");
+                if (StartDate > EndDate) return BadRequest("Start date cannot be after end date");
 
-            WeatherDateRangeData data = await WeatherAPIParentDateRange.Query(OpenWeatherMapDateRange.GetInstance(), StartDate, EndDate, Longitude, Latitude, TempUnit);
+                WeatherDateRangeData data = await WeatherAPIParentDateRange.Query(OpenWeatherMapDateRange.GetInstance(), StartDate, EndDate, Longitude, Latitude, TempUnit);
 
-            if (data is not null) return Ok(data);
-            else return NotFound("Error 404: The selected API may be temporarily unavailable");
+                if (data is not null) return Ok(data);
+                else return NotFound("Error 404: The selected API may be temporarily unavailable");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest(e.Message);
+            }
         }
     }
 }
